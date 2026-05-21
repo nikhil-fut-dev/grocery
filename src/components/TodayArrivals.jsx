bed857c3b37546c1eb4d0130b4db0337dc58a633
ANCHAL MAURYA <anchal.tech.pro@gmail.com>
2026-05-22 02:15:27 +0530

    modified:   src/components/TodayArrivals.jsx

--- src/components/TodayArrivals.jsx
+++ src/components/TodayArrivals.jsx
@@ -28,6 +28,55 @@ export default function TodayArrivals() {
   };
 
   return (
-    
+    <section className="product-sec pb-5">
+      <div className="container">
+        <h2 className="title text-center">Today Arrivals Fresh</h2>
+
+        <div className="row mt-5">
+          <div className="col-lg-4">
+            <div className="product-timer">
+              <img src="/images/item-time-2.jpg" alt="Arrival Deals Panel" />
+              <div className="product-time">
+                <div className="pr-time-text">
+                  <h4 className="text-white">Deals of the Week</h4>
+                </div>
+                <div className="timer-flex">
+                  <ul>
+                    <li>
+                      25 <span>DAYS</span>
+                    </li>
+                    <li>
+                      16 <span>HRS</span>
+                    </li>
+                    <li>
+                      20 <span>MIN</span>
+                    </li>
+                    <li>
+                      12 <span>SEC</span>
+                    </li>
+                  </ul>
+                </div>
+                <p className="text-white mt-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nikhil, molestiae!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-8">
+            <div className="product-slider-2 mt-5">
+              <Slider {...settings}>
+                {arrivals.map((product, index) => (
+                  <div key={index} className="px-2">
+                    <ProductCard {...ProductCard} />
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
