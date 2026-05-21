78e555e0e7d7c6c821f6cd720cf98583d1bc3d12
NIKHIL <nikhil.fut.dev@gmail.com>
2026-05-22 03:03:24 +0530

    modified:   src/components/PromoBanner.jsx

---
+++ src/components/PromoBanner.jsx
@@ -0,0 +1,35 @@
+export default function PromoBanner() {
+  return (
+    <section
+      className="bg-sec mt-5"
+      style={{ backgroundImage: "url(/images/Untitled-1.jpg)" }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6"></div>
+          <div className="col-lg-6">
+            <div className="bg-text">
+              <h3>Eat Healthy, Look Young, Feels Good!</h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur optio nesciunt et? Facere quo animi id totam
+                dignissimos quam dolore eos, harum minus!Voluptatem nemo
+                aspernatur dolorem repllendus quidem, odio dolor illum deserunt
+                harum placeat!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                molestias ea, ipsa voluptates provident minus cumque quo
+                dignissimos! Laudantium excepturi porro, sapiente unde fugit
+                maiores.
+              </p>
+              <div className="nav-btn mt-4">
+                <a href="#shop">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
