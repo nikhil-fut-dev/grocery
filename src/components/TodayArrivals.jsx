dc1adae6b977416d22f5f320ed517d364e6f08a3
NIKHIL <nikhil.fut.dev@gmail.com>
2026-05-22 02:24:18 +0530

    modified:   src/components/TodayArrivals.jsx

--- src/components/TodayArrivals.jsx
+++ src/components/TodayArrivals.jsx
@@ -3,14 +3,14 @@ import ProductCard from "./ProductCard";
 
 export default function TodayArrivals() {
   const arrivals = [
-    { title: "Banana", image: "product-img-6.png", price: "$22.00" },
-    { title: "Potato", image: "product-img-1.png", price: "$22.00" },
-    { title: "Spinach", image: "product-img-2.png", price: "$22.00" },
-    { title: "Milk", image: "product-img-3.png", price: "$22.00" },
-    { title: "Lime", image: "iten-1.jpg", price: "$22.00" },
-    { title: "Onion", image: "iten-2.jpg", price: "$22.00" },
-    { title: "Broccoli", image: "iten-3.jpg", price: "$22.00" },
-    { title: "Capsicum", image: "iten-4.jpg", price: "$22.00" },
+    { title: "Banana", image: "product-img-6.png", price: "22.00" },
+    { title: "Potato", image: "product-img-1.png", price: "22.00" },
+    { title: "Spinach", image: "product-img-2.png", price: "22.00" },
+    { title: "Milk", image: "product-img-3.png", price: "22.00" },
+    { title: "Lime", image: "iten-1.jpg", price: "22.00" },
+    { title: "Onion", image: "iten-2.jpg", price: "22.00" },
+    { title: "Broccoli", image: "iten-3.jpg", price: "22.00" },
+    { title: "Capsicum", image: "iten-4.jpg", price: "22.00" },
   ];
 
   const settings = {
