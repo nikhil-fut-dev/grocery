ce24e0f287c87c047ce3105fa67851f307ad95f6
NIKHIL <nikhil.fut.dev@gmail.com>
2026-05-22 00:53:27 +0530

    modified:   src/components/WeekendDeals.jsx

---
+++ src/components/WeekendDeals.jsx
@@ -0,0 +1,20 @@
+export default function WeekenDeals() {
+  return (
+    <section className="weekend-sec py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <div
+              className="weekend-main"
+              style={{ background: "url(/images/weekend-3-back.jpg" }}
+            >
+                <div className="weekend-text">
+                    <h5>weekend</h5>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
